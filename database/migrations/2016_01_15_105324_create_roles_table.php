e65349bf90dbecda4c201a7edad6a76e9a65cf7b
triabagus <triatop9@gmail.com>
2019-08-28 21:19:33 +0700

    initial commit

--- database/migrations/2016_01_15_105324_create_roles_table.php
+++ database/migrations/2016_01_15_105324_create_roles_table.php
@@ -3,9 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use jeremykenedy\LaravelBlocker\App\Models\BlockedType;
 
-class CreateLaravelBlockerTypesTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,16 +13,17 @@ class CreateLaravelBlockerTypesTable extends Migration
      */
     public function up()
     {
-        $blocked = new BlockedType();
-        $connection = $blocked->getConnectionName();
-        $table = $blocked->getTableName();
+        $connection = config('roles.connection');
+        $table = config('roles.rolesTable');
         $tableCheck = Schema::connection($connection)->hasTable($table);
 
         if (!$tableCheck) {
             Schema::connection($connection)->create($table, function (Blueprint $table) {
-                $table->increments('id');
-                $table->string('slug')->unique();
+                $table->increments('id')->unsigned();
                 $table->string('name');
+                $table->string('slug')->unique();
+                $table->string('description')->nullable();
+                $table->integer('level')->default(1);
                 $table->timestamps();
                 $table->softDeletes();
             });
@@ -37,10 +37,8 @@ class CreateLaravelBlockerTypesTable extends Migration
      */
     public function down()
     {
-        $blockedType = new BlockedType();
-        $connection = $blockedType->getConnectionName();
-        $table = $blockedType->getTableName();
-
+        $connection = config('roles.connection');
+        $table = config('roles.rolesTable');
         Schema::connection($connection)->dropIfExists($table);
     }
 }
